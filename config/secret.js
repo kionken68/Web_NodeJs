84a647c9e31ced967afd0d302cea096372f290d5
Nguyễn Tuấn <Nguyn Tun>
2016-05-09 16:09:38 +0700

    Video-41

---
+++ config/secret.js
@@ -0,0 +1,5 @@
+module.exports={
+  database:'mongodb://root:123456@ds013931.mlab.com:13931/ecommerce_vn',
+  port:3000,
+  secretKey:"AnhTuan"
+}
---
+++ test.js
@@ -0,0 +1,32 @@
+var async=require('async');
+
+
+Category.find({},function(err,category){
+  Product.findOne({category:category._id},function(err,productSingle){
+  Product.findById({_id:ProductSingle._id},function(err,productSingleById){
+
+    });
+  });
+});
+
+async.waterfall([
+  function(callback){
+    Category.find({},function(err,category){
+      if(err) return next(err);
+      callback(null, category)
+    });
+  },
+  function(category,callback){
+    Product.findOne({category:category._id},function(err,productSingle){
+      if(err)return next(err);
+      callback(null, productSingle)
+    });
+  },
+  function(productSingle,callback){
+    Product.findById({_id:ProductSingle._id},function(err,product){
+      if(err)return next(err);
+      res.render('');
+      res.redirect;
+    });
+  },
+])
